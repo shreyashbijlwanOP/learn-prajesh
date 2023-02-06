a43d95766a587a34059b57ff2b8571980cd9e381
shreyashbijlwanOP <shreyash.bijlwanmail@gmail.com>
2023-02-06 20:39:41 +0530

    Stater Template done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react';
 import './App.css';
 
 function App() {
   
-  let [state,setState] = useState(0)
-  
+
   console.log("Rendering")
   return (
     <div className="App">
       <h1>this is App </h1>
-      <button onClick={()=>{setState(state+1)}} >Increase State</button>
     </div>
   );
 }
